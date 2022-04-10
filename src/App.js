5b19a4ac2a4f4ffd4aff78eaaff55af6170d4e5b
osmanj@uoguelph.ca <osmanj@youremail.com>
2022-04-09 20:34:51 -0400

    basic ui

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let oneScore = 0;
+  let twoScore = 0;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <div className ="Grid">
+      </div>
+      <header className = "Score">
+        <div className = "P1Score">
+          <p>Player one Score
+          <br/>{twoScore}</p>
+        </div>
+        <div className = "P2Score">
+          <p>Player two Score
+          <br/>{oneScore}</p>
+        </div>
+        <button className = "Button"> New Game</button>
       </header>
     </div>
   );
